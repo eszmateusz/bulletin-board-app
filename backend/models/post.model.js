cabf0db61dab344f83cb85f17d7e1cb0a031a11d
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-05-10 21:35:45 +0200

    Add backend files

---
+++ backend/models/post.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  mail: { type: String, required: true },
+  date: { type: Date, required: true },
+  updateDate: { type: Date, required: true },
+  status: { type: String, required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String },
+  price: { type: Number },
+  phone: { type: String },
+  location: { type: String },
+});
+
+module.exports = mongoose.model('Post', postSchema);
